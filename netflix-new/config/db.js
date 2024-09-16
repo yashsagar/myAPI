debcd0fb9f6d484396228e0f7d3bc7b742132cb2
yashsagar <sagar.sagar75@gmail.com>
2024-09-16 22:48:13 +0530

    fix-1

--- netflix-new/config/db.js
+++ netflix-new/config/db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-import { ENV_VARS } from "./envVars.js";
 
 export const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(ENV_VARS.MONGO_URL);
+    const connection = await mongoose.connect(
+      "mongodb://localhost:27017/netflix-new-db"
+    );
     console.log("mongoDB connected: " + connection.connection.host);
   } catch (error) {
     console.error("error connecting to MONGODB: " + error.message);
